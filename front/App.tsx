d7735f8b2432d018b9f835347df9143fb1d68798
JISUCHOIGIT <wltn203@naver.com>
2025-03-23 18:33:02 +0900

    frist commit

---
+++ front/App.tsx
@@ -0,0 +1,48 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+   <SafeAreaView style = {styles.container}>
+    <View style = {styles.inputContainer}>    
+    <TextInput style = {styles.input}></TextInput>
+    <Text>텍스트</Text>
+    </View>
+    <View style = {styles.inputContainer}>    
+    <TextInput style = {styles.input}></TextInput>
+    <Text>텍스트</Text>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : 'yellow',
+  },
+  input : {
+    flex : 1,
+    borderWidth : 2,
+    borderColor : 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer : {
+    flex : 1,
+    backgroundColor : 'red',
+    flexDirection : 'row',
+    alignItems: 'center',
+  }
+});
+
+export default App;
